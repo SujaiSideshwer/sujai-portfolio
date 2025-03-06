df683879bfe20b73237bb676b94ccf9fb5038d36
Sujai Sideshwer <sujaisideshwer17@gmail.com>
2025-03-06 19:21:40 +0530

    change to vite.config.js to counter 404 error

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/sujai-portfolio",
+  base: "/sujai-portfolio/",
 });
